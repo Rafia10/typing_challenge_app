5091d6b2ce72cb392cd657b7f0944007ce9272bb
Rafia-Qadir <you@example.com>
2022-06-13 06:18:48 -0400

    Angular Typing App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { lorem } from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing_app';
+  randomText = lorem.sentence();
+  enteredText = '';
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.enteredText = input.value;
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
